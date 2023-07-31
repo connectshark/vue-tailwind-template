b61c4be547a87c2561b1d9ca2d267ccb483b6d42
connectshark <46649021+connectshark@users.noreply.github.com>
2023-07-31 04:47:46 +0000

    Deploying to gh-pages from @ connectshark/vue-tailwind-template@d84679c6992c274d28cedd51d69157cd03ff4f3c 🚀

---
+++ assets/index-4863d180.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-a4c992dc.js";const o={},s={class:"text-white text-3xl"};function n(r,_){return t(),c("h1",s,"Hi")}const i=e(o,[["render",n]]);export{i as default};
