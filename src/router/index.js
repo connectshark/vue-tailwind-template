1f3310187ea14f9148a4bef8491099239eedf236
Chambers <bobhus394@gmail.com>
2023-07-31 12:04:20 +0800

    add auto router plugin

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,7 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router/auto'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    }
-  ]
+  history: createWebHashHistory(import.meta.env.BASE_URL)
 })
 
 export default router
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
 
-// https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VueRouter()],
   server: {
     port: 8080
   },
