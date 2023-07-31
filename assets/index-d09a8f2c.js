26f14faf0c93330c83a08094d77ced42c5de2983
connectshark <46649021+connectshark@users.noreply.github.com>
2023-07-31 04:04:53 +0000

    Deploying to gh-pages from @ connectshark/vite-tailwind-template@1f3310187ea14f9148a4bef8491099239eedf236 🚀

---
+++ assets/index-d09a8f2c.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-7f04b0a2.js";const o={},s={class:"text-white text-3xl"};function n(r,_){return t(),c("h1",s,"Hi")}const i=e(o,[["render",n]]);export{i as default};
