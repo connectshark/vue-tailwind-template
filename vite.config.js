00e2170bf2ed4c5e2109496a8d6f5659630771fd
Chambers <bobhus394@gmail.com>
2026-03-09 15:23:25 +0800

    update vue router version

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
-import VueRouter from 'unplugin-vue-router/vite'
+import VueRouter from 'vue-router/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 /** @type {import('vite').UserConfig} */
